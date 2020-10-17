16b6fe415c744376628fa0acc4b99de3b560b0ff
JulianNavi <39164188+JulianNavi@users.noreply.github.com>
2020-10-17 15:06:44 +0300

    add slider score and round score

--- BullsEyes/ContentView.swift
+++ BullsEyes/ContentView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var alertIsVisible: Bool = false
-    @State var sliderValue: Double = 50.0
+    @State var alertIsVisible = false
+    @State var sliderValue = 50.0
+    @State var target = Int.random(in: 1...100)
     
     var body: some View{
         VStack {
@@ -20,14 +21,14 @@ struct ContentView: View {
             //target row
             HStack {
                 Text("Put the bullseye as close as you can to:")
-                Text("100")
+                Text("\(target)")
             }
             // slider row
             
             Spacer()
             HStack{
                 Text("1")
-                Slider(value: self.$sliderValue, in: 1...100)
+                Slider(value: $sliderValue, in: 1...100)
                 Text("100")
             }
             //button row
@@ -40,8 +41,8 @@ struct ContentView: View {
                         .fontWeight(.bold)
                 }
                 .alert(isPresented: $alertIsVisible) { () -> Alert in
-                    var roundedValue: Int = Int(self.sliderValue.rounded())
-                    return Alert(title: Text("Hello!"), message: Text("There slider's value is \(roundedValue)"), dismissButton: .default(Text("Osom!")))
+                    return Alert(title: Text("Hello!"), message: Text(
+                        "There slider's value is \(sliderValueRounded()).\n" + "You scored \(pointsForCurrentRound()) points in this round."), dismissButton: .default(Text("Osom!")))
                 }
             Spacer()
             
@@ -64,6 +65,14 @@ struct ContentView: View {
             }
             .padding(.bottom, 20)
         }
+    }
+    
+    func sliderValueRounded() -> Int {
+        return Int(sliderValue.rounded())
+    }
+    
+    func pointsForCurrentRound() -> Int {
+        return 100 - abs(target - sliderValueRounded())
     }
     
 }
